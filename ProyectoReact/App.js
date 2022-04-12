e2c2b1d2871354c6ac2309be5485563be9cffead
jubaal <jubaal2122daw2@protonmail.com>
2022-04-12 17:17:57 +0200

    prueba de imagen

--- ProyectoReact/App.js
+++ ProyectoReact/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image style={styles.image} source = {require("./assets/logo.png")}/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +14,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f52f2f',
     alignItems: 'center',
     justifyContent: 'center',
+    color: 'white'
   },
+  image: {
+    marginBottom: 40,
+    width: 300,
+    height: 300,
+  }
 });
