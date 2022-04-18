0e4a8f894e5d484ad4fb97af4a77fd47bf656e26
jubaal <jubaal2122daw2@protonmail.com>
2022-04-18 18:25:30 +0200

    funciona modal con props

--- ProyectoReact/components/MapComponent.js
+++ ProyectoReact/components/MapComponent.js
@@ -50,85 +50,48 @@ const styles = StyleSheet.create({
   },
 });
 
-const AbrirModalComponent = () => {
-  // const [modalVisible, setModalVisible] = useState(false);
-  console.log("entra");
-  return(
-    <View style={styles.centeredView}>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}>
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      <Pressable style={[styles.button, styles.buttonOpen]} onPress={() => setModalVisible(true)}>
-        <Text style={styles.textStyle}>Show Modal</Text>
-      </Pressable>
-    </View>
-  );
-}
-
 const MapComponent = (props) => {
   const puntoMapa = [];
   const [modalVisible, setModalVisible] = useState(false);
-
-  for (let prop of props["cinesBDD"]) {
-
-    const punto = <MapView.Marker
-      key={prop["id"]}
-      coordinate={{
-        latitude: prop["lat"],
-        longitude: prop["long"]
-      }}
-    // title={ prop["nombre"].toString() }
-    // description={prop["valoracion"].toString() }
-    >
-      <MapView.Callout onPress={() => {setModalVisible(true)}}>
-        <Text>{prop["nombre"]}</Text>
-        <Text>{prop["valoracion"]}</Text>
-        <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Hello World!</Text>
-            <Pressable
-              style={[styles.button, styles.buttonClose]}
-              onPress={() => setModalVisible(!modalVisible)}>
-              <Text style={styles.textStyle}>Hide Modal</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-      </MapView.Callout>
-    </MapView.Marker>;
-    puntoMapa.push(punto);
-  }
+  const [propHook, setPropHook] = useState({});
   return (
     <View style={styles.container}>
-      <MapView style={styles.map}
-        showsMyLocationButton={true}
-        showsUserLocation={true}>
-        {puntoMapa}
-      </MapView>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>Hello World!</Text>
+              <Pressable
+                style={[styles.button, styles.buttonClose]}
+                onPress={() => setModalVisible(!modalVisible)}>
+                <Text style={styles.textStyle}>Hide Modal</Text>
+              </Pressable>
+            </View>
+          </View>
+        </Modal>
+        <MapView style={styles.map}
+          showsMyLocationButton={true}
+          showsUserLocation={true}>
+          {props["cinesBDD"].map(prop => (
+            <MapView.Marker
+              key={prop["id"]}
+              coordinate={{
+                latitude: prop["lat"],
+                longitude: prop["long"],
+              }}
+              title={prop["nombre"]}
+              description={prop["valoracion"].toString()}>
+              <MapView.Callout onPress={() => { setModalVisible(true), setPropHook({ prop }) }} >
+              </MapView.Callout>
+            </MapView.Marker>
+          ))}
+        </MapView>
     </View>
   );
 }
