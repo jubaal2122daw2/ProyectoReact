fb3ab06d33a72fce3858ee59c3c1eb142c9754ae
jubaal <jubaal2122daw2@protonmail.com>
2022-04-18 15:46:33 +0200

    el modal va pocho

--- ProyectoReact/components/MapComponent.js
+++ ProyectoReact/components/MapComponent.js
@@ -1,36 +1,136 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MapView from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Alert, Modal, Pressable } from 'react-native';
 
 const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
 });
+
+const AbrirModalComponent = () => {
+  // const [modalVisible, setModalVisible] = useState(false);
+  console.log("entra");
+  return(
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable style={[styles.button, styles.buttonOpen]} onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
+    </View>
+  );
+}
 
 const MapComponent = (props) => {
   const puntoMapa = [];
-  for(let prop of props["cinesBDD"]){
+  const [modalVisible, setModalVisible] = useState(false);
+
+  for (let prop of props["cinesBDD"]) {
+
     const punto = <MapView.Marker
-        key={prop["id"]}
-        coordinate={{
-          latitude: prop["lat"],
-          longitude: prop["long"]
-        }}
-        title={ prop["nombre"].toString() }
-        description={prop["valoracion"].toString() }
-        />;
+      key={prop["id"]}
+      coordinate={{
+        latitude: prop["lat"],
+        longitude: prop["long"]
+      }}
+    // title={ prop["nombre"].toString() }
+    // description={prop["valoracion"].toString() }
+    >
+      <MapView.Callout onPress={() => {setModalVisible(true)}}>
+        <Text>{prop["nombre"]}</Text>
+        <Text>{prop["valoracion"]}</Text>
+        <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      </MapView.Callout>
+    </MapView.Marker>;
     puntoMapa.push(punto);
   }
-    return (
-      <View style={styles.container}>
-        <MapView style={styles.map}
-          showsMyLocationButton={true}
-          showsUserLocation={true}>
-            {puntoMapa}
-        </MapView>
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map}
+        showsMyLocationButton={true}
+        showsUserLocation={true}>
+        {puntoMapa}
+      </MapView>
+    </View>
+  );
 }
-export {MapComponent};
+
+export { MapComponent };
